7c87de135bd84eed6a99c43f4e3ad386a5110094
Juan Carlos Asensio <jasensio@ufm.edu>
2023-11-25 18:51:35 -0500

    Start implementing basic file-based caching

--- server/app.js
+++ server/app.js
@@ -1,9 +1,38 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const fetch = require('node-fetch');
 
 const app = express();
 const port = 3000;
+
+/**
+ * HELPER METHODS
+ */
+
+const pathToData = path.resolve(__dirname, '../data', 'externalLinks.json');
+
+const readFile = (callback, returnJson = false, filePath = pathToData, encoding = 'utf8') => {
+    fs.readFile(filePath, encoding, (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        callback(returnJson ? JSON.parse(data) : data);
+    });
+
+};
+
+const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+
+    fs.writeFile(filePath, fileData, encoding, (err) => {
+        if (err) {
+            throw err;
+        }
+
+        callback();
+    });
+};
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
@@ -12,22 +41,50 @@ app.get('/', (req, res) => {
 });
 
 app.get('/random-concert', async (req, res) => {
-  // const tinyDeskWikipediaURL = 'https://en.wikipedia.org/wiki/List_of_Tiny_Desk_Concerts';
-
-  const URL = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=List_of_Tiny_Desk_Concerts&formatversion=2'
-
+  const URL = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=List_of_Tiny_Desk_Concerts&formatversion=2';
   const wikipediaRes = await fetch(URL);
   const data = await wikipediaRes.json();
 
-  const dataProperties = Object.keys(data.parse);
+  // Useful for debugging...
+  // console.log(data.parse.externallinks);
 
-  // const regex = /https\:\/\/www.npr.org\/\d{4}\/\d{2}\/\d{2}\//g
+  const now = new Date(Date.now().toString());
 
-  const externalLinks = data.parse.externallinks.filter(link => link.includes('tiny-desk-concert') && link.startsWith('https://www.npr.org/'));
+  const newData = readFile((data) => {
+    res.send(data.externalLinks);
+  }, true);
 
-  res.send(externalLinks)
+  // const externalLinks = 
+  //   data.parse.externallinks
+  //     .filter(link => (
+  //       link.includes('tiny-desk-concert') && 
+  //       link.startsWith('https://www.npr.org/')
+  //   ));
+
+  // res.send(externalLinks)
 })
 
+/**
+ *  
+ * 
+ *  Fetch-and-set step: 
+ *  Fetch wikipedia data...
+ *  store it in a file
+ *  Time stamp that file
+ *  
+ *  Request-handle step:
+ *  When request comes in, check if data is stale (this can be optional for now)
+ *  If data is not stale: get data from file in memory...
+ *  Else: do Fetch-and-set
+ * 
+ *  Response step:
+ * 
+ *  Return non-stale data in response
+ *  
+ * 
+ *    
+ */
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}/`)
 })
