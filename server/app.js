725a3d6a8ea3328eec0da366ef4dd85d6940e2e7
Juan Carlos Asensio <jasensio@ufm.edu>
2023-11-26 17:15:05 -0500

    Use async/await for file read/data fetch

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const util = require('util');
 const express = require('express');
 const path = require('path');
 const fetch = require('node-fetch');
@@ -7,21 +8,25 @@ const app = express();
 const port = 3000;
 
 /**
+ * VARIABLES
+ */
+
+/**
  * HELPER METHODS
  */
 
 const pathToData = path.resolve(__dirname, '../data', 'externalLinks.json');
 
-const readFile = (callback, returnJson = false, filePath = pathToData, encoding = 'utf8') => {
-    fs.readFile(filePath, encoding, (err, data) => {
-        if (err) {
-            throw err;
-        }
+// const readFile = (callback, returnJson = false, filePath = pathToData, encoding = 'utf8') => {
+//     fs.readFile(filePath, encoding, (err, data) => {
+//         if (err) {
+//             throw err;
+//         }
 
-        callback(returnJson ? JSON.parse(data) : data);
-    });
+//         callback(returnJson ? JSON.parse(data) : data);
+//     });
 
-};
+// };
 
 const writeFile = (fileData, callback, filePath = pathToData, encoding = 'utf8') => {
 
@@ -34,7 +39,38 @@ const writeFile = (fileData, callback, filePath = pathToData, encoding = 'utf8')
     });
 };
 
+const readFile = util.promisify(fs.readFile);
+
+const getCachedData = async () => {
+  try {
+    const data = await readFile(pathToData);
+    return JSON.parse(data);
+    
+  } catch (error) {
+    res.status(500).send(`
+      The following error occurred when reading the file at ${pathToData}: 
+      ${error.message}
+    `);
+  }
+};
+
+const isConcertLink = (link) => (
+  (link.includes('tiny-desk-concert') || link.includes('tiny-desk-concert')) && 
+  link.startsWith('https://www.npr.org/') &&
+  !link.includes('series') &&
+  !link.includes('sections')
+);
+
+/** 
+ * MIDDLEWARE
+ */
+
 app.use(express.static(path.resolve(__dirname, '../public')));
+
+
+/**
+ * ROUTES
+ */
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
@@ -46,40 +82,35 @@ app.get('/random-concert', async (req, res) => {
   // Useful for debugging...
   // console.log(data.parse.externallinks);
 
+  // How will we be using dates to check if data is too old or is stale?
   // const now = new Date(Date.now().toString());
 
-  readFile(async (data) => {
-    if (data?.isDataStale) {
-      console.log('data is STALE')
+  // Can we use async/await to clean up code and catch errors appropriately?
+  const cachedData = await getCachedData();
 
-      const wikipediaRes = await fetch(URL);
-      const fetchedData = await wikipediaRes.json();
+  if (!cachedData?.isDataStale) {
+    console.log('data is FRESH');
+    const filteredLinks = cachedData?.externallinks.filter(isConcertLink);
 
-      const dataToCacheAndSend = {
-        isDataStale: false, 
-        externallinks: [...fetchedData.parse.externallinks]
-      };
+    res.send(filteredLinks);
+  
+  } else {
+    console.log('data is STALE')
 
-      writeFile(JSON.stringify(dataToCacheAndSend, null, 2), () => {
-        res.send(dataToCacheAndSend.externallinks);
-      });
-      
-    } else {
-      console.log('data is FRESH');
-      
-      console.log(data)
-      res.send(data?.externallinks);
-    }
-  }, true);
+    const wikipediaRes = await fetch(URL);
+    const fetchedData = await wikipediaRes.json();
 
-  // const externalLinks = 
-  //   data.parse.externallinks
-  //     .filter(link => (
-  //       link.includes('tiny-desk-concert') && 
-  //       link.startsWith('https://www.npr.org/')
-  //   ));
+    const filteredLinks = fetchedData.parse.externallinks.filter(isConcertLink);
 
-  // res.send(externalLinks)
+    const dataToCacheAndSend = {
+      isDataStale: false, 
+      externallinks: [...filteredLinks]
+    };
+
+    writeFile(JSON.stringify(dataToCacheAndSend, null, 2), () => {
+      res.send(dataToCacheAndSend.externallinks);
+    });
+  }
 })
 
 /**
