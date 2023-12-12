8071393cd20cea018298120072ef26008dcc895e
Juan Carlos Asensio <jasensio@ufm.edu>
2023-12-12 10:07:51 -0600

    Test build steps

---
+++ pages/hello.js
@@ -0,0 +1,16 @@
+function helloWorld() {
+  const html =`
+    <html>
+      <head></head>
+      <body>
+        <ul>
+          <li>This is it, A TEST!</li>
+        </ul>
+      </body>
+    </html>
+  `
+  console.log(html);
+  return html;
+}
+
+helloWorld();
