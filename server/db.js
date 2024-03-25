fd66384098ed4ca88ca197572084c2c59c1494de
Juan Carlos Asensio <jasensio@ufm.edu>
2024-03-25 15:16:39 -0400

    Coerce DB url to string

--- server/db.js
+++ server/db.js
@@ -10,7 +10,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DB_URL
+  databaseURL: `${process.env.FIREBASE_DB_URL}`
 });
 
 // As an admin, the app has access to read and write all data, regardless of Security Rules
