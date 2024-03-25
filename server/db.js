ee896cc0529be24834c2f1896def2237b9d3ae12
Juan Carlos Asensio <jasensio@ufm.edu>
2024-03-25 15:22:28 -0400

    Use env var again

--- server/db.js
+++ server/db.js
@@ -6,11 +6,12 @@ dotenv.config();
 
 // Setting up Firebase Admin SDK https://firebase.google.com/docs/database/admin/start
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const FIREBASE_DB_URL = toString(process.env.FIREBASE_DB_URL);
 
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://tiny-random-concert-default-rtdb.firebaseio.com/'
+  databaseURL: toString(FIREBASE_DB_URL)
 });
 
 // As an admin, the app has access to read and write all data, regardless of Security Rules
