2ee0c9922cd4e8129878149d2a11ca95142c8b35
Juan Carlos Asensio <jasensio@ufm.edu>
2024-06-24 15:32:24 -0400

    Cond.render link vs. maxed-out msg

--- server/cron.js
+++ server/cron.js
@@ -26,15 +26,13 @@ export const cron = async (req, res) => {
       const { revid: wpRevid } = wpData.parse;
       const allWPLinks = wpData.parse.externallinks.filter(isConcertLink);
       const wpLinksCount = allWPLinks.length;
-      const newLinks = allWPLinks.slice(currentLinksCount, allWPLinks.length)
+      const newLinks = allWPLinks.slice(currentLinksCount, wpLinksCount);
 
       if (newLinks.length === wpLinksCount - currentLinksCount) {
         setRevid(wpRevid);
         setCount(wpLinksCount)
         setConcertsLinks(newLinks)
 
-        // Perhaps be more explicit with response status code: 300 or something that indicates an update has happened?
-        // need to read docs to check what Vercel expects to be returned from cron job.
         res.status(200).json({
           success: true,
           message: 'Data has been updated.'
