ad6dcc71ba34defbeb666e060729c42e696df043
Juan Carlos Asensio <37816105+juancarlosasensio@users.noreply.github.com>
2024-06-24 15:42:15 -0400

    f/protect api routes (#19)
    * Cleanup
    
    * Auth /api routes
    
    * Add rate limiter
    
    * Middware for actual ip (and not reverse proxy)
    
    * Server log clientIp from
    
    * Send req maxed out msg from server
    
    * Cond.render link vs. maxed-out msg
    
    * Add comment

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 import dotenv from 'dotenv';
-import express from 'express';
 import path from 'path';
+import express from 'express';
+import basicAuth from 'express-basic-auth';
+import { rateLimit } from 'express-rate-limit';
+import { mw } from 'request-ip';
 import { __dirname, __filename } from './utils.js';
-import { getRandConcert, getAllData } from './db.js';
-import { cron } from './cron.js'
+import { getRandConcert } from './db.js';
+import { cron as cronRouteHandler } from './cron.js';
+import { router as apiRouter } from './api.js';
 
 /**
- * Config and Constants
+ * CONFIG/CONSTANTS
  */
 dotenv.config();
 const app = express();
@@ -18,56 +22,60 @@ const port = 3000;
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.set("views", path.resolve(__dirname, '../views'));
 app.set('view engine', 'ejs');
+app.use(mw());
 
-
-// Cron job route handler
-app.use('/cron', cron);
+const limiter = rateLimit({
+  windowMs: 24 * 3600 * 1000, // 24 hours
+	limit: 5,
+  keyGenerator: (req, res) => req.clientIp, // See: https://stackoverflow.com/questions/64188573/express-rate-limit-blocking-requests-from-all-users
+  status: 200,
+  handler: (req, res, next) => {
+    res.TRCData = {};
+    res.TRCData.message = `
+      You've maxed out the number of random concerts for today. 
+      Please try again tomorrow :)
+    `
+    next(); // See: https://dev.to/brunohgv/limiting-node-js-api-calls-with-express-rate-limit-11kl#:~:text=const%20apiRequestLimiter%20%3D%20rateLimit(%7B%0A%20%20%20%20windowMs%3A%201%20*%2060%20*%201000%2C%20//%201%20minute%0A%20%20%20%20max%3A%202%2C%20//%20limit%20each%20IP%20to%202%20requests%20per%20windowMs%0A%20%20%20%20handler%3A%20function%20(req%2C%20res%2C%20next)%20%7B%0A%20%20%20%20%20%20applyFeesForConsumer()%0A%20%20%20%20%20%20next()%0A%20%20%20%20%7D%0A%7D)
+  }
+})
 
 /**
- * Routes
+ * ROUTE HANDLERS
  */
-app.get('/', async (req, res) => { 
-    try {
-      const randLink = await getRandConcert();
-      
+
+// Cron job handler
+app.use('/cron', cronRouteHandler);
+
+// API router with basic auth middleware
+app.use("/api", basicAuth({ users: { 'admin': process.env.API_PSWD }}), apiRouter);
+
+// Content
+app.get('/', limiter, async (req, res) => {
+    if (res?.TRCData?.message) {
       res.render('index.ejs', {
-        concertLink: randLink
+        concertLink: null,
+        serverMsg: res?.TRCData?.message
       });
-    } catch (error) {
-      res.status(500).send(`
-        The following error occurred: ${error.message}
-    `);
+    } else {
+          try {
+            const randLink = await getRandConcert();
+            console.log('randLink', randLink);
+            
+            res.render('index.ejs', {
+              concertLink: randLink,
+              serverMsg: null
+            });
+          } catch (error) {
+            res.status(500).send(`
+              The following error occurred: ${error.message}
+          `);
+        }
     }
 });
 
-app.get('/api/concerts', async (req, res) => {
-  let data = {};
-  try {
-    data = {...await getAllData()}
-    
-    res.header("Content-Type",'application/json');
-    res.send(JSON.stringify(data, null, 4));
-
-  } catch (error) {
-    res.status(500).send(`
-      The following error occurred: 
-      ${error.message}
-    `);
-  }
-})
-
-app.get('/api/random-concert', async (req, res) => {
-  try {
-    const randLink = await getRandConcert();
-    res.json(randLink)
-
-  } catch (error) {
-    res.status(500).send(`
-      The following error occurred: 
-      ${error.message}
-    `);
-  }
-})
+/**
+ * LISTEN ON PORT
+ */
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}/`)
